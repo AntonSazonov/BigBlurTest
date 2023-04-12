d5f5650b85f45b6eb3e9222020a1ebe48f140ceb
Anton <anton.sazonov.dev@gmail.com>
2023-04-12 16:39:00 +0300

    Added cmake batch files for Windows

---
+++ _cmake_clang_dbg.cmd
@@ -0,0 +1,6 @@
+@echo off
+cls
+echo.
+cmake -G Ninja -B _build_clang_dbg . -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_BUILD_TYPE=Debug && ^
+cmake --build _build_clang_dbg
+if %errorlevel% NEQ 0 ( del /f main.exe 2> nul )

87ef924e7418c9afb39604434262903c41d8d099
Anton <anton.sazonov.dev@gmail.com>
2023-04-13 12:25:31 +0300

    Cleanup

--- src/agg/agg_recursive_blur_mt.hpp
+++ src/agg/agg_recursive_blur_mt.hpp
@@ -109,54 +109,6 @@ namespace agg {
             pixfmt_transposer <Img> img2( img );
             blur_x( img2, radius, parallel_for );
         }
-#if 0
-    private:
-        agg::pod_vector<calculator_type> m_sum1;
-        agg::pod_vector<calculator_type> m_sum2;
-        agg::pod_vector<color_type>      m_buf;
-#endif
     };
-
-#if 0
-    //=================================================recursive_blur_calc_rgba
-    template<class T=double> struct recursive_blur_calc_rgba {
-        typedef T value_type;
-        typedef recursive_blur_calc_rgba<T> self_type;
-
-        value_type r,g,b,a;
-
-        template<class ColorT> 
-        AGG_INLINE void from_pix(const ColorT& c) {
-            r = c.r;
-            g = c.g;
-            b = c.b;
-            a = c.a;
-        }
-
-        AGG_INLINE void calc(value_type b1, 
-                             value_type b2, 
-                             value_type b3, 
-                             value_type b4,
-                             const self_type& c1, 
-                             const self_type& c2, 
-                             const self_type& c3, 
-                             const self_type& c4)
-        {
-            r = b1*c1.r + b2*c2.r + b3*c3.r + b4*c4.r;
-            g = b1*c1.g + b2*c2.g + b3*c3.g + b4*c4.g;
-            b = b1*c1.b + b2*c2.b + b3*c3.b + b4*c4.b;
-            a = b1*c1.a + b2*c2.a + b3*c3.a + b4*c4.a;
-        }
-
-        template<class ColorT> 
-        AGG_INLINE void to_pix(ColorT& c) const {
-            typedef typename ColorT::value_type cv_type;
-            c.r = (cv_type)uround(r);
-            c.g = (cv_type)uround(g);
-            c.b = (cv_type)uround(b);
-            c.a = (cv_type)uround(a);
-        }
-    };
-#endif
 
 } // namespace agg

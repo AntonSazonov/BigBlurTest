173b7ffaba7798ab0009b3e7f343405e5ff056a5
Anton <anton.sazonov.dev@gmail.com>
2023-04-13 10:29:41 +0300

    Cleanup

--- src/san_rgba_calc_naive.hpp
+++ src/san_rgba_calc_naive.hpp
@@ -6,7 +6,9 @@ class naive {
 	int	c[4];
 
 public:
-	naive() { zero(); }
+	naive() {
+		c[0] = c[1] = c[2] = c[3] = 0;
+	}
 
 	naive( int x, int y, int z, int w ) {
 		c[0] = x;
@@ -22,7 +24,12 @@ public:
 		c[0] =  value        & 0xff;
 	}
 
-	void zero() { c[0] = c[1] = c[2] = c[3] = 0; }
+	operator uint32_t () const {
+		return ((c[3] & 0xff) << 24) |
+			   ((c[2] & 0xff) << 16) |
+			   ((c[1] & 0xff) <<  8) |
+			    (c[0] & 0xff);
+	}
 
 	naive & operator = ( uint32_t value ) {
 		c[3] = (value >> 24) & 0xff;
@@ -37,22 +44,6 @@ public:
 		c[1] += rhs.c[1];
 		c[2] += rhs.c[2];
 		c[3] += rhs.c[3];
-		return *this;
-	}
-
-	naive & operator += ( uint32_t value ) {
-		c[3] += (value >> 24) & 0xff;
-		c[2] += (value >> 16) & 0xff;
-		c[1] += (value >>  8) & 0xff;
-		c[0] +=  value        & 0xff;
-		return *this;
-	}
-
-	naive & operator -= ( uint32_t value ) {
-		c[3] -= (value >> 24) & 0xff;
-		c[2] -= (value >> 16) & 0xff;
-		c[1] -= (value >>  8) & 0xff;
-		c[0] -=  value        & 0xff;
 		return *this;
 	}
 
@@ -80,6 +71,8 @@ public:
 			c[3] / value );
 	}
 
+#if 0
+	// May be will be used later...
 	naive operator >> ( uint8_t value ) const {
 		return naive(
 			c[0] >> value,
@@ -87,13 +80,7 @@ public:
 			c[2] >> value,
 			c[3] >> value );
 	}
-
-	explicit operator uint32_t () const {
-		return ((c[3] & 0xff) << 24) |
-			   ((c[2] & 0xff) << 16) |
-			   ((c[1] & 0xff) <<  8) |
-			    (c[0] & 0xff);
-	}
+#endif
 }; // struct naive
 
 } // namespace san::rgba_calc

5b62b84234df45974e48ee89cd763786ea12b56e
Anton <anton.sazonov.dev@gmail.com>
2023-04-13 16:30:03 +0300

    ...

--- _cmake_clang_dbg.cmd
+++ _cmake_clang_dbg.cmd
@@ -3,4 +3,4 @@ cls
 echo.
 cmake -G Ninja -B _build_clang_dbg . -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_BUILD_TYPE=Debug && ^
 cmake --build _build_clang_dbg
-if %errorlevel% NEQ 0 ( del /f main.exe 2> nul )
+if %errorlevel% NEQ 0 ( del /f main-Clang-Debug.exe 2> nul )

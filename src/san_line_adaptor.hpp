99b7080c619202fe340be675867494521bb22667
Anton <anton.sazonov.dev@gmail.com>
2023-04-18 12:18:40 +0300

    Move line_adaptor to separate file

---
+++ src/san_line_adaptor.hpp
@@ -0,0 +1,28 @@
+#pragma once
+
+namespace san {
+
+// Horizontal or vertical line adaptor
+class line_adaptor {
+	uint32_t *	m_ptr;
+	int			m_len;
+	int			m_advance;
+
+public:
+	line_adaptor( uint32_t * p_src, int len, int advance ) :
+		m_ptr( p_src ), m_len( len ), m_advance( advance ) {}
+
+	uint32_t *	ptr()			{ return m_ptr; }
+	int			advance() const	{ return m_advance; }
+
+	uint32_t get_pix( int i ) const {
+		if ( i < 0 ) i = 0; else if ( i >= m_len ) i = m_len - 1;
+		return m_ptr[i * m_advance];
+	}
+
+	void set_pix( int i, uint32_t value ) {
+		m_ptr[i * m_advance] = value;
+	}
+}; // struct line_adaptor
+
+} // namespace san

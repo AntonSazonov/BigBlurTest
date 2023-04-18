c3dfe5773ae0602564d5e77745e984d1efb2ea92
Anton <anton.sazonov.dev@gmail.com>
2023-04-18 21:57:59 +0300

    Set optimization level in command line to not overload GitHub's build action

--- _cmake_clang_dbg.cmd
+++ _cmake_clang_dbg.cmd
@@ -1,6 +1,6 @@
 @echo off
 cls
 echo.
-cmake -G Ninja -B _build_clang_dbg . -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_BUILD_TYPE=Debug && ^
+cmake -G Ninja -B _build_clang_dbg . -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_BUILD_TYPE=Debug -DDEBUG_OPT_LEVEL="-O1" && ^
 cmake --build _build_clang_dbg
 if %errorlevel% NEQ 0 ( del /f main-Clang-Debug.exe 2> nul )

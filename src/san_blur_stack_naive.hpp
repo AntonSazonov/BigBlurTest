8ccb307053d91714b25bb0a3636e394b6a774320
SAN <anton.sazonov.dev@gmail.com>
2023-04-26 18:40:24 +0300

    naive_line_process -> naive_do_line

--- src/san_blur_stack_naive.hpp
+++ src/san_blur_stack_naive.hpp
@@ -3,7 +3,7 @@
 namespace san::blur::stack {
 
 template <typename NaiveCalcT>
-void naive_line_process( adaptor::straight_line & line, int beg, int end/*exclusive*/, int radius ) {
+void naive_do_line( adaptor::straight_line & line, int beg, int end/*exclusive*/, int radius ) {
 	int den = radius * (radius + 2) + 1;
 	int div = radius * 2 + 1;
 	uint32_t * p_stack = (uint32_t *)SAN_STACK_ALLOC( sizeof( uint32_t ) * div );
@@ -25,7 +25,6 @@ void naive_line_process( adaptor::straight_line & line, int beg, int end/*exclus
 
 	line.set_pix_start( beg );
 	for ( int i = beg; i < end; i++ ) {
-//		line.set_pix( i, uint32_t( sum / den ) );
 		line.set_pix_next( uint32_t( sum / den ) );
 
 		sum -= sum_out;
@@ -58,7 +57,7 @@ void naive( san::surface_view & image, ParallelForT & parallel_for, int radius, 
 	parallel_for.run_and_wait( 0, image.height(), [&]( int a, int b ) {
 		for ( int y = a; y < b; y++ ) {
 			adaptor::straight_line line( (uint32_t *)image.row_ptr( y ), image.width(), 1 );
-			naive_line_process<NaiveCalcT>( line, 0, image.width(), radius );
+			naive_do_line<NaiveCalcT>( line, 0, image.width(), radius );
 		}
 	}, override_num_threads );
 
@@ -66,7 +65,7 @@ void naive( san::surface_view & image, ParallelForT & parallel_for, int radius, 
 	parallel_for.run_and_wait( 0, image.width(), [&]( int a, int b ) {
 		for ( int x = a; x < b; x++ ) {
 			adaptor::straight_line line( (uint32_t *)image.col_ptr( x ), image.height(), image.stride() / image.components() );
-			naive_line_process<NaiveCalcT>( line, 0, image.height(), radius );
+			naive_do_line<NaiveCalcT>( line, 0, image.height(), radius );
 		}
 	}, override_num_threads );
 }
--- src/san_blur_stack_simd_naive.hpp
+++ src/san_blur_stack_simd_naive.hpp
@@ -3,7 +3,7 @@
 namespace san::blur::stack::simd {
 
 template <typename SIMDCalcT>
-void naive_line_process( adaptor::straight_line & line, int beg, int end/*exclusive*/, int radius ) {
+void naive_do_line( adaptor::straight_line & line, int beg, int end/*exclusive*/, int radius ) {
 	int div = radius * 2 + 1;
 	uint32_t * p_stack = (uint32_t *)SAN_STACK_ALLOC( sizeof( uint32_t ) * div );
 
@@ -24,8 +24,10 @@ void naive_line_process( adaptor::straight_line & line, int beg, int end/*exclus
 	}
 
 	int i_stack = radius;
+
+	line.set_pix_start( beg );
 	for ( int i = beg; i < end; i++ ) {
-		line.set_pix( i, uint32_t( sum / divisor ) );
+		line.set_pix_next( uint32_t( sum / divisor ) );
 
 		sum -= sum_out;
 
@@ -57,7 +59,7 @@ void naive( san::surface_view & image, ParallelForT & parallel_for, int radius, 
 	parallel_for.run_and_wait( 0, image.height(), [&]( int a, int b ) {
 		for ( int y = a; y < b; y++ ) {
 			adaptor::straight_line line( (uint32_t *)image.row_ptr( y ), image.width(), 1 );
-			naive_line_process<SIMDCalcT>( line, 0, image.width(), radius );
+			naive_do_line<SIMDCalcT>( line, 0, image.width(), radius );
 		}
 	}, override_num_threads );
 
@@ -65,7 +67,7 @@ void naive( san::surface_view & image, ParallelForT & parallel_for, int radius, 
 	parallel_for.run_and_wait( 0, image.width(), [&]( int a, int b ) {
 		for ( int x = a; x < b; x++ ) {
 			adaptor::straight_line line( (uint32_t *)image.col_ptr( x ), image.height(), image.stride() / image.components() );
-			naive_line_process<SIMDCalcT>( line, 0, image.height(), radius );
+			naive_do_line<SIMDCalcT>( line, 0, image.height(), radius );
 		}
 	}, override_num_threads );
 }
